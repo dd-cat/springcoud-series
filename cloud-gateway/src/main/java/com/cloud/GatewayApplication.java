fcd20b50ce8fd926a0e1f31b90630e2a983f1915
admin <17639627374@163.com>
2021-04-27 11:08:12 +0800

    gateway初使用

---
+++ cloud-gateway/src/main/java/com/cloud/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @description:
+ * @author: dd-cat
+ * @date:2021/4/26 15:25
+ * @version:1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
